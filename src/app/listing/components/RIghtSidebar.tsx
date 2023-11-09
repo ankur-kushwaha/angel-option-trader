e72c85af45b69a303c09d767085e4d6f8347b21e
Ankur Kushwaha <akushwaha@adobe.com>
2023-11-09 11:45:25 +0530

    code fix

--- src/app/listing/components/RIghtSidebar.tsx
+++ src/app/listing/components/RIghtSidebar.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react'
 
 
 export default function RightSidebar() {
-
+  const searchRef = React.useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
   const router = useRouter()
   // const { data: optionsData, deleteItem } = useStore();
@@ -22,11 +22,12 @@ export default function RightSidebar() {
   }
 
   function handleAdd(){
-    let stockCode = (document.getElementById("default-search") as HTMLInputElement).value;
+    let stockCode = searchRef.current?.value||"";
+    if(!stockCode) return;
     let newOptionStocks = [...new Set([...optionStocks,stockCode])]  
     window.localStorage.setItem('OptionStocks', JSON.stringify(newOptionStocks));
     setOptionStocks(newOptionStocks);
-    (document.getElementById("default-search") as HTMLInputElement).value = "";
+    searchRef.current!.value = "";
   }
 
 
@@ -47,7 +48,7 @@ export default function RightSidebar() {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
             </div>
-            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+            <input ref={searchRef} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
             <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleAdd}>Add</button>
           </div>
         </div>
